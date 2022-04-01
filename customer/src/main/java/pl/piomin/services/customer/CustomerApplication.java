adb916a1af09a34cdbefd1130984af18839ce738
Simon Schneider <simon.schneider@tuhh.de>
2022-04-01 11:17:40 +0200

    corrected branch

--- customer/src/main/java/pl/piomin/services/customer/CustomerApplication.java
+++ customer/src/main/java/pl/piomin/services/customer/CustomerApplication.java
@@ -1,15 +1,17 @@
-package pl.piomin.services.account;
+package pl.piomin.services.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class AccountService {
+@EnableFeignClients
+public class CustomerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AccountService.class, args);
+		SpringApplication.run(CustomerApplication.class, args);
 	}
 
 }
